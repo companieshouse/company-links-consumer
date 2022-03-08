b6a9693ecf116aa0c792b6bac6c959d2d3764200
iichr <ciliev@companieshouse.gov.uk>
2022-03-08 16:19:39 +0000

    Call get company profile in the insovlency stream processor + service and processor adjustments

--- src/main/java/uk/gov/companieshouse/company/links/processor/InsolvencyStreamProcessor.java
+++ src/main/java/uk/gov/companieshouse/company/links/processor/InsolvencyStreamProcessor.java
@@ -1,31 +1,49 @@
 package uk.gov.companieshouse.company.links.processor;
 
+import static uk.gov.companieshouse.company.links.processor.ResponseHandler.handleResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.api.company.CompanyProfile;
+import uk.gov.companieshouse.api.delta.Insolvency;
+import uk.gov.companieshouse.api.delta.InsolvencyDelta;
+import uk.gov.companieshouse.api.model.ApiResponse;
 import uk.gov.companieshouse.company.links.exception.RetryErrorException;
 import uk.gov.companieshouse.company.links.producer.InsolvencyStreamProducer;
+import uk.gov.companieshouse.company.links.service.CompanyProfileService;
 import uk.gov.companieshouse.delta.ChsDelta;
+import uk.gov.companieshouse.logging.Logger;
 
 
 @Component
 public class InsolvencyStreamProcessor {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(InsolvencyStreamProcessor.class);
+    private final Logger logger;
     private final InsolvencyStreamProducer insolvencyStreamProducer;
+    private final CompanyProfileService companyProfileService;
 
+    /**
+     * Construct an insolvency stream processor.
+     */
     @Autowired
-    public InsolvencyStreamProcessor(InsolvencyStreamProducer insolvencyStreamProducer) {
+    public InsolvencyStreamProcessor(InsolvencyStreamProducer insolvencyStreamProducer,
+                                     CompanyProfileService companyProfileService,
+                                     Logger logger) {
         this.insolvencyStreamProducer = insolvencyStreamProducer;
+        this.companyProfileService = companyProfileService;
+        this.logger = logger;
     }
 
     /**
-     * process.
+     * Process CHS Delta message.
      */
     //TODO What model should we use here? Is it a Avro?
     public void process(Message<ChsDelta> chsDelta) {
@@ -37,7 +55,28 @@ public class InsolvencyStreamProcessor {
             //TODO We need to create a new one for this processor
             final boolean isRetry = headers.containsKey("INSOLVENCY_DELTA_RETRY_COUNT");
             final ChsDelta payload = chsDelta.getPayload();
+            final String logContext = payload.getContextId();
+            final Map<String, Object> logMap = new HashMap<>();
 
+            ObjectMapper mapper = new ObjectMapper();
+            InsolvencyDelta insolvencyDelta = mapper.readValue(payload.getData(),
+                    InsolvencyDelta.class);
+            logger.trace(String.format("InsolvencyDelta extracted "
+                    + "from Kafka message: %s", insolvencyDelta));
+
+            // We always receive only one insolvency per delta from CHIPS
+            Insolvency insolvency = insolvencyDelta.getInsolvency().get(0);
+            final String companyNumber = insolvency.getCompanyNumber();
+
+            final ApiResponse<CompanyProfile> response =
+                    companyProfileService.getCompanyProfile(logContext, companyNumber);
+            logger.trace(String.format("Retrieved company profile for company number %s: %s",
+                    companyNumber, response.getData()));
+            handleResponse(null, HttpStatus.valueOf(response.getStatusCode()), logContext,
+                    "Response from GET call to company profile api", logMap, logger);
+
+            // TODO check if company needs updating - use response.getData()
+            // TODO PATCH company profile
         } catch (RetryErrorException ex) {
             retry(chsDelta);
         } catch (Exception ex) {
@@ -53,5 +92,4 @@ public class InsolvencyStreamProcessor {
     private void handleErrors(Message<ChsDelta> chsDelta) {
 
     }
-
 }
---
+++ src/main/java/uk/gov/companieshouse/company/links/service/CompanyProfileService.java
@@ -0,0 +1,74 @@
+package uk.gov.companieshouse.company.links.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.companieshouse.api.InternalApiClient;
+import uk.gov.companieshouse.api.company.CompanyProfile;
+import uk.gov.companieshouse.api.http.ApiKeyHttpClient;
+import uk.gov.companieshouse.api.http.HttpClient;
+import uk.gov.companieshouse.api.model.ApiResponse;
+import uk.gov.companieshouse.company.links.service.api.BaseApiClientServiceImpl;
+import uk.gov.companieshouse.logging.Logger;
+
+@Service
+public class CompanyProfileService extends BaseApiClientServiceImpl {
+
+    @Value("${api.company-profile-api-key}")
+    private String companyProfileApiKey;
+
+    @Value("${api.endpoint}")
+    private String companyProfileApiUrl;
+
+    /**
+     * Construct a company profile service - used to retrieve a company profile record.
+     * @param logger the CH logger
+     */
+    @Autowired
+    public CompanyProfileService(Logger logger) {
+        super(logger);
+    }
+
+    /**
+     * Retrieve a company profile given a company number from company-profile-api.
+     *
+     * @param companyNumber the company's company number
+     * @return an ApiResponse containing the CompanyProfileApi data model
+     */
+    public ApiResponse<CompanyProfile> getCompanyProfile(String contextId, String companyNumber) {
+        String uri = String.format("/company/%s", companyNumber);
+
+        Map<String, Object> logMap = createLogMap(companyNumber, "GET", uri);
+        logger.infoContext(contextId, String.format("GET %s", uri), logMap);
+
+        return executeOp(contextId, "getCompanyProfileApi", uri,
+                getApiClient(contextId)
+                        .privateCompanyResourceHandler()
+                        .getCompanyProfile(uri));
+    }
+
+    /**
+     * Get an internal api client instance.
+     */
+    public InternalApiClient getApiClient(String contextId) {
+        InternalApiClient apiClient = new InternalApiClient(getHttpClient(contextId));
+        apiClient.setBasePath(companyProfileApiUrl);
+        return apiClient;
+    }
+
+    private HttpClient getHttpClient(String contextId) {
+        ApiKeyHttpClient httpClient = new ApiKeyHttpClient(companyProfileApiKey);
+        httpClient.setRequestId(contextId);
+        return httpClient;
+    }
+
+    private Map<String, Object> createLogMap(String companyNumber, String method, String path) {
+        final Map<String, Object> logMap = new HashMap<>();
+        logMap.put("company_number", companyNumber);
+        logMap.put("method", method);
+        logMap.put("path", path);
+        return logMap;
+    }
+}

898b5ccd8c5eb1d27c813b62de8f7ed8dec2d7fe
gseregni-ch <98030779+gseregni-ch@users.noreply.github.com>
2022-06-16 15:28:21 +0100

    DSND-1128 - Addressing issue with 410 GONE that was returning retryable exception (#67)

--- src/main/java/uk/gov/companieshouse/company/links/processor/StreamResponseProcessor.java
+++ src/main/java/uk/gov/companieshouse/company/links/processor/StreamResponseProcessor.java
@@ -1,6 +1,9 @@
 package uk.gov.companieshouse.company.links.processor;
 
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.Map;
+import java.util.Set;
 import org.springframework.http.HttpStatus;
 import uk.gov.companieshouse.company.links.exception.NonRetryableErrorException;
 import uk.gov.companieshouse.company.links.exception.RetryableErrorException;
@@ -32,6 +35,38 @@ public class StreamResponseProcessor {
         logMap.put("company_number", companyNumber);
 
         if (HttpStatus.BAD_REQUEST == httpStatus) {
+            logger.errorContext(logContext, message, null, logMap);
+            throw new NonRetryableErrorException(message);
+        } else if (httpStatus.is2xxSuccessful()) {
+            logger.info(String.format("Successfully invoked %s %s endpoint"
+                            + " for message with contextId %s and company number %s",
+                    requestType, apiType, logContext, companyNumber));
+        } else {
+            logger.errorContext(logContext, message, null, logMap);
+            throw new RetryableErrorException(message);
+        }
+    }
+
+    /**
+     * Common response handler.
+     */
+    void handleDeleteResponse(
+            final HttpStatus httpStatus,
+            final String logContext,
+            String requestType,
+            ApiType apiType,
+            String companyNumber,
+            final Map<String, Object> logMap)
+            throws NonRetryableErrorException, RetryableErrorException {
+        var message = String.format("Response from %s %s", requestType, apiType.toString());
+        logMap.put("status", httpStatus.toString());
+        logMap.put("company_number", companyNumber);
+
+        Set<HttpStatus> nonRetryableStatuses =
+                Collections.unmodifiableSet(EnumSet.of(
+                        HttpStatus.BAD_REQUEST, HttpStatus.GONE));
+
+        if (nonRetryableStatuses.contains(httpStatus)) {
             logger.errorContext(logContext, message, null, logMap);
             throw new NonRetryableErrorException(message);
         } else if (httpStatus.is2xxSuccessful()) {

6cc07c48b804b7b5ca188db8e06270dc9cbc7377
crowlandsCH <crowlands@companieshouse.gov.uk>
2022-12-09 16:45:37 +0000

    Implement delete exemptions client test class

--- src/test/java/uk/gov/companieshouse/company/links/service/DeleteExemptionsClientTest.java
+++ src/test/java/uk/gov/companieshouse/company/links/service/DeleteExemptionsClientTest.java
@@ -1,5 +1,134 @@
 package uk.gov.companieshouse.company.links.service;
 
-public class DeleteExemptionsClientTest {
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpResponseException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.api.InternalApiClient;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.handler.company.PrivateCompanyLinksResourceHandler;
+import uk.gov.companieshouse.api.handler.company.links.request.PrivateCompanyExemptionsLinksDelete;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.model.ApiResponse;
+import uk.gov.companieshouse.company.links.exception.NonRetryableErrorException;
+import uk.gov.companieshouse.company.links.exception.RetryableErrorException;
+import uk.gov.companieshouse.logging.Logger;
 
+import java.util.Collections;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteExemptionsClientTest {
+
+    private static final String PATH = "/company/12345678/links/exemptions";
+
+    @Mock
+    private Supplier<InternalApiClient> internalApiClientSupplier;
+
+    @Mock
+    private InternalApiClient internalApiClient;
+
+    @Mock
+    private PrivateCompanyLinksResourceHandler resourceHandler;
+
+    @Mock
+    private PrivateCompanyExemptionsLinksDelete exemptionsLinksDelete;
+
+    @Mock
+    private Logger logger;
+
+    @InjectMocks
+    private DeleteExemptionsClient client;
+
+    @Test
+    void testDelete() throws ApiErrorResponseException, URIValidationException {
+        // given
+        when(internalApiClientSupplier.get()).thenReturn(internalApiClient);
+        when(internalApiClient.privateCompanyLinksResourceHandler()).thenReturn(resourceHandler);
+        when(resourceHandler.deleteExemptionsCompanyLink(anyString())).thenReturn(exemptionsLinksDelete);
+        when(exemptionsLinksDelete.execute()).thenReturn(new ApiResponse<>(200, Collections.emptyMap()));
+
+        // when
+        client.deleteExemptionsLink(PATH);
+
+        // then
+        verify(resourceHandler).deleteExemptionsCompanyLink(PATH);
+        verify(exemptionsLinksDelete).execute();
+    }
+
+    @Test
+    void testThrowNonRetryableExceptionIfClientErrorReturned() throws ApiErrorResponseException, URIValidationException {
+        // given
+        when(internalApiClientSupplier.get()).thenReturn(internalApiClient);
+        when(internalApiClient.privateCompanyLinksResourceHandler()).thenReturn(resourceHandler);
+        when(resourceHandler.deleteExemptionsCompanyLink(anyString())).thenReturn(exemptionsLinksDelete);
+        when(exemptionsLinksDelete.execute()).thenThrow(new ApiErrorResponseException(new HttpResponseException.Builder(404, "Not found", new HttpHeaders())));
+
+        // when
+        client.deleteExemptionsLink(PATH);
+
+        // then
+        verify(resourceHandler).deleteExemptionsCompanyLink(PATH);
+        verify(exemptionsLinksDelete).execute();
+    }
+
+    @Test
+    void testThrowRetryableExceptionIfServerErrorReturned() throws ApiErrorResponseException, URIValidationException {
+        // given
+        when(internalApiClientSupplier.get()).thenReturn(internalApiClient);
+        when(internalApiClient.privateCompanyLinksResourceHandler()).thenReturn(resourceHandler);
+        when(resourceHandler.deleteExemptionsCompanyLink(anyString())).thenReturn(exemptionsLinksDelete);
+        when(exemptionsLinksDelete.execute()).thenThrow(new ApiErrorResponseException(new HttpResponseException.Builder(500, "Internal server error", new HttpHeaders())));
+
+        // when
+        Executable actual = () -> client.deleteExemptionsLink(PATH);
+
+        // then
+        assertThrows(RetryableErrorException.class, actual);
+        verify(resourceHandler).deleteExemptionsCompanyLink(PATH);
+        verify(exemptionsLinksDelete).execute();
+    }
+
+    @Test
+    void testThrowRetryableExceptionIfIllegalArgumentExceptionIsCaught() throws ApiErrorResponseException, URIValidationException {
+        // given
+        when(internalApiClientSupplier.get()).thenReturn(internalApiClient);
+        when(internalApiClient.privateCompanyLinksResourceHandler()).thenReturn(resourceHandler);
+        when(resourceHandler.deleteExemptionsCompanyLink(anyString())).thenReturn(exemptionsLinksDelete);
+        when(exemptionsLinksDelete.execute()).thenThrow(new IllegalArgumentException("Internal server error"));
+
+        // when
+        Executable actual = () -> client.deleteExemptionsLink(PATH);
+
+        // then
+        assertThrows(RetryableErrorException.class, actual);
+        verify(resourceHandler).deleteExemptionsCompanyLink(PATH);
+        verify(exemptionsLinksDelete).execute();
+    }
+
+    @Test
+    void testThrowNonRetryableExceptionIfPathInvalid() throws ApiErrorResponseException, URIValidationException {
+        // given
+        when(internalApiClientSupplier.get()).thenReturn(internalApiClient);
+        when(internalApiClient.privateCompanyLinksResourceHandler()).thenReturn(resourceHandler);
+        when(resourceHandler.deleteExemptionsCompanyLink(anyString())).thenReturn(exemptionsLinksDelete);
+        when(exemptionsLinksDelete.execute()).thenThrow(new URIValidationException("Invalid URI"));
+
+        // when
+        Executable actual = () -> client.deleteExemptionsLink("/invalid/path");
+
+        // then
+        assertThrows(NonRetryableErrorException.class, actual);
+        verify(resourceHandler).deleteExemptionsCompanyLink("/invalid/path");
+        verify(exemptionsLinksDelete).execute();
+    }
 }

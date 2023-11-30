3ddfd14ac2933ea563bcec082be8ffa81b23a993
sthompsonCH <sthompson@companieshouse.gov.uk>
2023-11-30 11:35:19 +0000

    DSND-2231: Make java change to trigger new build
    * A change has been made by a github admin to get the bot working.
      This commit is to test that change and make sure the bot
      decoration is showing on GitHub.

--- src/main/java/uk/gov/companieshouse/company/links/SonarQube.java
+++ src/main/java/uk/gov/companieshouse/company/links/SonarQube.java
@@ -2,6 +2,7 @@ package uk.gov.companieshouse.company.links;
 
 public class SonarQube {
     public int calculate(int first, int second) {
-        return first + second;
+        int result = first + second;
+        return result;
     }
 }

058b2edc16d66709589953fbe9ec33afaaf30265
crowlandsCH <crowlands@companieshouse.gov.uk>
2024-02-14 13:22:36 +0000

    DSND-2375: Checkstyle.

--- src/main/java/uk/gov/companieshouse/company/links/exception/RetryableErrorException.java
+++ src/main/java/uk/gov/companieshouse/company/links/exception/RetryableErrorException.java
@@ -10,13 +10,16 @@ public class RetryableErrorException extends RuntimeException {
         super(message, exception);
     }
 
-    public RetryableErrorException(Exception exception) {
-        super(exception);
-    }
-
+    /**
+     * Additional constructor to avoid code duplication.
+     *
+     * @param companyNumber CompanyNumber
+     */
     public RetryableErrorException(String companyNumber, String linkType, Exception exception) {
-        super(String.format("Company profile [%s] does not exist when processing add %s link request",
-                companyNumber, linkType), exception);
+        super(String.format(
+                "Company profile [%s] does not exist when processing add %s link request",
+                        companyNumber, linkType),
+                exception);
     }
 }
 
